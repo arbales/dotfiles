ecc8f936277d4517a9f5ce7dc14ccf701e09856c
Austin Bales <austin@manymoon.com>
2011-06-20 20:22:28 -0700

    Updated Node and RVM

--- node/npm.zsh
+++ node/npm.zsh
@@ -1,2 +1,2 @@
-export PATH=":/usr/local/Cellar/node/0.1.100/bin:/usr/local/share/npm/bin:$PATH"
-export NODE_PATH=/usr/local/lib/node:$NODE_PATH
+#export PATH=":/usr/local/Cellar/node/0.1.100/bin:/usr/local/share/npm/bin:$PATH"
+export NODE_PATH=/usr/local/lib/node:/usr/local/lib/node_modules:$NODE_PATH
