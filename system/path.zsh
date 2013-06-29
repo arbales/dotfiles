da82b14de9551c4bf4d80531381118c642f2e175
Austin Bales <arbales@gmail.com>
2013-06-28 23:06:21 -0400

    Use rbenv instead of rvm.

--- system/path.zsh
+++ system/path.zsh
@@ -1,3 +1,3 @@
 export PATH=".:bin:/usr/local/bin:/usr/local/sbin:$HOME/.sfs:$ZSH/bin:$PATH"
-
+if which rbenv > /dev/null; then eval "$(rbenv init -)"; fi
 export MANPATH="/usr/local/man:/usr/local/mysql/man:/usr/local/git/man:$MANPATH"
