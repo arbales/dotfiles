049c0bc5698b4912b8cf5aa9668c7a0c0285218e
Austin Bales <arbales@fb.com>
2020-02-05 13:49:12 -0800

    rbenv update

--- system/path.zsh
+++ system/path.zsh
@@ -1,3 +1,6 @@
 export PATH=".:bin:/usr/local/bin:/usr/local/sbin:$HOME/.sfs:$ZSH/bin:$PATH"
-if which rbenv > /dev/null; then eval "$(rbenv init -)"; fi
+
+# The Postgres app
+export PATH="$PATH:/Applications/Postgres.app/Contents/Versions/9.4/bin"
+
 export MANPATH="/usr/local/man:/usr/local/mysql/man:/usr/local/git/man:$MANPATH"
