1a5faa97999d1c9a468e5a72a24519e7d2d8c10c
Austin Bales <arbales@gmail.com>
2021-12-29 23:26:35 -0800

    mac updates

--- homebrew/readline.zsh
+++ homebrew/readline.zsh
@@ -1,3 +1,7 @@
-  export LDFLAGS="-L/usr/local/opt/readline/lib"
-  export CPPFLAGS="-I/usr/local/opt/readline/include"
-  export PKG_CONFIG_PATH="/usr/local/opt/readline/lib/pkgconfig"
+  # export LDFLAGS="-L/usr/local/opt/readline/lib"
+  # export CPPFLAGS="-I/usr/local/opt/readline/include"
+  # export PKG_CONFIG_PATH="/usr/local/opt/readline/lib/pkgconfig"
+
+  export LDFLAGS="-L/opt/homebrew/opt/readline/lib"
+  export CPPFLAGS="-I/opt/homebrew/opt/readline/include"
+  export PKG_CONFIG_PATH="/opt/homebrew/opt/readline/lib/pkgconfig"
--- system/path.zsh
+++ system/path.zsh
@@ -4,3 +4,6 @@ export PATH=".:bin:/usr/local/bin:/usr/local/sbin:$HOME/.sfs:$ZSH/bin:$PATH"
 export PATH="$PATH:/Applications/Postgres.app/Contents/Versions/latest/bin"
 
 export MANPATH="/usr/local/man:/usr/local/mysql/man:/usr/local/git/man:$MANPATH"
+
+# Homebrew Setup in Opt
+eval "$(/opt/homebrew/bin/brew shellenv)"
